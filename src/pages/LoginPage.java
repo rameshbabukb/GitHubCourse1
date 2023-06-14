94ed53d0e8cd22201c85a79a225c6f8d722bd05a
rameshbabukb <rameshven2053@gmail.com>
2023-06-14 15:44:34 +0530

    added/updated/deleted file

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,6 +3,9 @@ package pages;
 public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("Hello LoginPage....");
+		String un="admin";
+		String pwd="admin";
+		System.out.println("Logged In .....");
 	}
 
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class SearchPage {
+	public void Search()
+	{
+	}
+	
+
+}
